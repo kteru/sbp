2118fe19f04f4d8b664edf5f5a0f7a84081d4de0
teru <teru@kteru.net>
2017-11-13 16:54:06 +0900

    Implement MSG_AGE_CORRECTIONS

---
+++ msg_age_corrections.go
@@ -0,0 +1,35 @@
+package sbp
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+// MsgAgeCorrections represents a contents of MSG_AGE_CORRECTIONS.
+type MsgAgeCorrections struct {
+	// GPS Time of Week (unit:ms)
+	Tow uint32
+
+	// Age of the corrections (0xFFFF indicates in-valid)
+	Age uint16
+}
+
+func (m *MsgAgeCorrections) FromBytes(bs []byte) error {
+	if len(bs) < 6 {
+		return io.ErrUnexpectedEOF
+	}
+
+	m.Tow = binary.LittleEndian.Uint32(bs[0:4])
+	m.Age = binary.LittleEndian.Uint16(bs[4:6])
+
+	return nil
+}
+
+func (m *MsgAgeCorrections) Bytes() ([]byte, error) {
+	bs := make([]byte, 6)
+
+	binary.LittleEndian.PutUint32(bs[0:4], m.Tow)
+	binary.LittleEndian.PutUint16(bs[4:6], m.Age)
+
+	return bs, nil
+}
