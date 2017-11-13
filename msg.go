a6a76fd3451bb52433f947b20841cc6ebc0b318d
teru <teru@kteru.net>
2017-11-13 18:34:41 +0900

    Update MessageIDs

--- msg.go
+++ msg.go
@@ -6,23 +6,33 @@ import "errors"
 const (
 	// Logging
 	TypeMsgLog uint16 = 0x0401
+	TypeMsgFwd uint16 = 0x0402
 
 	// Navigation
-	TypeMsgGpsTime         uint16 = 0x0100
-	TypeMsgDops            uint16 = 0x0206
-	TypeMsgPosEcef         uint16 = 0x0200
-	TypeMsgPosLlh          uint16 = 0x0201
-	TypeMsgBaselineEcef    uint16 = 0x0202
-	TypeMsgBaselineNed     uint16 = 0x0203
-	TypeMsgVelEcef         uint16 = 0x0204
-	TypeMsgVelNed          uint16 = 0x0205
-	TypeMsgBaselineHeading uint16 = 0x0207
+	TypeMsgGpsTime         uint16 = 0x0102
+	TypeMsgUtcTime         uint16 = 0x0103
+	TypeMsgDops            uint16 = 0x0208
+	TypeMsgPosEcef         uint16 = 0x0209
+	TypeMsgPosLlh          uint16 = 0x020a
+	TypeMsgBaselineEcef    uint16 = 0x020b
+	TypeMsgBaselineNed     uint16 = 0x020c
+	TypeMsgVelEcef         uint16 = 0x020d
+	TypeMsgVelNed          uint16 = 0x020e
+	TypeMsgBaselineHeading uint16 = 0x020f
+	TypeMsgAgeCorrections  uint16 = 0x0210
 
 	// Observation
-	TypeMsgObs         uint16 = 0x0043
-	TypeMsgBasePosLlh  uint16 = 0x0044
-	TypeMsgBasePosEcef uint16 = 0x0048
-	TypeMsgEphemeris   uint16 = 0x0047
+	TypeMsgObs           uint16 = 0x004a
+	TypeMsgBasePosLlh    uint16 = 0x0044
+	TypeMsgBasePosEcef   uint16 = 0x0048
+	TypeMsgEphemerisGps  uint16 = 0x0086
+	TypeMsgEphemerisSbas uint16 = 0x0084
+	TypeMsgEphemerisGlo  uint16 = 0x0088
+	// TypeMsgIono               uint16 = 0x0090
+	// TypeMsgSvConfigurationGps uint16 = 0x0091
+	// TypeMsgGroupDelay         uint16 = 0x0093
+	// TypeMsgAlmanacGps         uint16 = 0x0070
+	// TypeMsgAlmanacGlo         uint16 = 0x0071
 
 	// Settings
 	TypeMsgSettingsSave            uint16 = 0x00a1
@@ -34,14 +44,17 @@ const (
 	TypeMsgSettingsReadByIndexDone uint16 = 0x00a6
 
 	// System
-	TypeMsgStartup   uint16 = 0xff00
-	TypeMsgHeartbeat uint16 = 0xffff
+	TypeMsgStartup     uint16 = 0xff00
+	TypeMsgDgnssStatus uint16 = 0xff02
+	TypeMsgHeartbeat   uint16 = 0xffff
 )
 
 // TypeToMsg is a map of constructors for Messages.
 var TypeToMsg = map[uint16]func() Msg{
 	TypeMsgLog:                     func() Msg { return new(MsgLog) },
+	TypeMsgFwd:                     func() Msg { return new(MsgFwd) },
 	TypeMsgGpsTime:                 func() Msg { return new(MsgGpsTime) },
+	TypeMsgUtcTime:                 func() Msg { return new(MsgUtcTime) },
 	TypeMsgDops:                    func() Msg { return new(MsgDops) },
 	TypeMsgPosEcef:                 func() Msg { return new(MsgPosEcef) },
 	TypeMsgPosLlh:                  func() Msg { return new(MsgPosLlh) },
@@ -50,10 +63,13 @@ var TypeToMsg = map[uint16]func() Msg{
 	TypeMsgVelEcef:                 func() Msg { return new(MsgVelEcef) },
 	TypeMsgVelNed:                  func() Msg { return new(MsgVelNed) },
 	TypeMsgBaselineHeading:         func() Msg { return new(MsgBaselineHeading) },
+	TypeMsgAgeCorrections:          func() Msg { return new(MsgAgeCorrections) },
 	TypeMsgObs:                     func() Msg { return new(MsgObs) },
 	TypeMsgBasePosLlh:              func() Msg { return new(MsgBasePosLlh) },
 	TypeMsgBasePosEcef:             func() Msg { return new(MsgBasePosEcef) },
-	TypeMsgEphemeris:               func() Msg { return new(MsgEphemeris) },
+	TypeMsgEphemerisGps:            func() Msg { return new(MsgEphemerisGps) },
+	TypeMsgEphemerisSbas:           func() Msg { return new(MsgEphemerisSbas) },
+	TypeMsgEphemerisGlo:            func() Msg { return new(MsgEphemerisGlo) },
 	TypeMsgSettingsSave:            func() Msg { return new(MsgSettingsSave) },
 	TypeMsgSettingsWrite:           func() Msg { return new(MsgSettingsWrite) },
 	TypeMsgSettingsReadReq:         func() Msg { return new(MsgSettingsReadReq) },
@@ -62,6 +78,7 @@ var TypeToMsg = map[uint16]func() Msg{
 	TypeMsgSettingsReadByIndexResp: func() Msg { return new(MsgSettingsReadByIndexResp) },
 	TypeMsgSettingsReadByIndexDone: func() Msg { return new(MsgSettingsReadByIndexDone) },
 	TypeMsgStartup:                 func() Msg { return new(MsgStartup) },
+	TypeMsgDgnssStatus:             func() Msg { return new(MsgDgnssStatus) },
 	TypeMsgHeartbeat:               func() Msg { return new(MsgHeartbeat) },
 }
 
